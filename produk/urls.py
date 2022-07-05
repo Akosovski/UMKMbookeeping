d7e303e0d2ae6c314f6e80c318be129002596509
Muhammad Akhta <akhtacaesar@gmail.com>
2022-07-06 00:12:41 +0700

    Produk Minor Rework
    Ubah masih Bug redirect ke Tambah Produk.

--- produk/urls.py
+++ produk/urls.py
@@ -5,4 +5,5 @@ from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
     path('produk', views.index, name="produk"),
     path('tambah-produk', views.tambah_produk, name="tambah-produk"),
+    path('ubah-produk/<int:id>', views.ubah_produk, name="ubah-produk"),
 ]
--- produk/views.py
+++ produk/views.py
@@ -36,11 +36,95 @@ def tambah_produk(request):
         buyprice = request.POST.get('buyprice')
         sellprice = request.POST.get('sellprice')
         description = request.POST.get('description')
+        stock = request.POST.get('stock')
         dateadded = request.POST.get('dateadded')
         dateupdated = request.POST.get('dateadded')
         vendor = request.POST.get('vendor')
 
-        Produk.objects.create(owner=request.user, name=name, buyprice=buyprice, sellprice=sellprice, description=description, dateadded=dateadded, dateupdated=dateadded, vendor=vendor)
+        if not name:
+            messages.error(request, 'Nama Produk perlu diisi.')
+            return render(request, 'produk/tambah_produk.html', context)
+
+        if not buyprice:
+            messages.error(request, 'Harga Beli perlu diisi.')
+            return render(request, 'produk/tambah_produk.html', context)
+
+        if not sellprice:
+            messages.error(request, 'Harga Jual perlu iisi.')
+            return render(request, 'produk/tambah_produk.html', context)
+
+        if not description:
+            messages.error(request, 'Deskripsi perlu diisi.')
+            return render(request, 'produk/tambah_produk.html', context)
+
+        if not stock:
+            messages.error(request, 'Stok perlu diisi.')
+            return render(request, 'produk/tambah_produk.html', context)
+
+        if not dateadded:
+            messages.error(request, 'Tanggal Pengadaan perlu diisi.')
+            return render(request, 'produk/tambah_produk.html', context)
+
+        # Date Updated Sama Dengan Date Added Karena Awal Produk Masuk
+        Produk.objects.create(owner=request.user, name=name, buyprice=buyprice, sellprice=sellprice, description=description, stock=stock, dateadded=dateadded, dateupdated=dateadded, vendor=vendor)
         messages.success(request, 'Penambahan Produk Sukses.')
+
+        return redirect('produk')
+
+@login_required(login_url = '/authentication/login')
+def ubah_produk(request, id):
+    produks = Produk.objects.get(pk=id)
+    vendors = Vendor.objects.all() 
+    context = {
+        'produks': produks,
+        'values': produks,
+        'vendors': vendors,
+    }
+    if request.method == 'GET':
+        return render(request, 'produk/ubah_produk.html', context)
+        
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        buyprice = request.POST.get('buyprice')
+        sellprice = request.POST.get('sellprice')
+        description = request.POST.get('description')
+        stock = request.POST.get('stock')
+        dateupdated = request.POST.get('dateupdated')
+        vendor = request.POST.get('vendor')
+
+        if not name:
+            messages.error(request, 'Nama Produk perlu diisi.')
+            return render(request, 'produk/ubah_produk.html', context)
+
+        if not buyprice:
+            messages.error(request, 'Harga Beli perlu diisi.')
+            return render(request, 'produk/ubah_produk.html', context)
+
+        if not sellprice:
+            messages.error(request, 'Harga Jual perlu iisi.')
+            return render(request, 'produk/ubah_produk.html', context)
+
+        if not description:
+            messages.error(request, 'Deskripsi perlu diisi.')
+            return render(request, 'produk/ubah_produk.html', context)
+
+        if not stock:
+            messages.error(request, 'Stok perlu diisi.')
+            return render(request, 'produk/ubah_produk.html', context)
+
+        if not dateupdated:
+            messages.error(request, 'Tanggal Perubahan perlu diisi.')
+            return render(request, 'produk/ubah_produk.html', context)
+        
+        produks.owner = request.user
+        produks.buyprice = buyprice
+        produks.sellprice = sellprice
+        produks.description = description
+        produks.stock = stock
+        produks.dateupdated = dateupdated
+        produks.vendor = vendor
+
+        produks.save()
+        messages.success(request, 'Perubahan Produk Sukses.')
 
         return redirect('produk')
