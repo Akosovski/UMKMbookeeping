ca6fa2bed3fb4906827992e27ccfe3e137822119
Muhammad Akhta <akhtacaesar@gmail.com>
2022-07-10 16:17:42 +0700

    Update settings.py

--- umkmbookeeping/settings.py
+++ umkmbookeeping/settings.py
@@ -134,9 +134,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'umkmbookeeping/static')]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 django_heroku.settings(locals())
 
 # Default primary key field type
