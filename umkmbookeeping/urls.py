8810d9844835005d680f3502c8070bbfa240098f
Muhammad Akhta <akhtacaesar@gmail.com>
2022-07-10 15:58:04 +0700

    Update urls.py

--- umkmbookeeping/urls.py
+++ umkmbookeeping/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', include('pembukuan.urls')),
@@ -7,4 +9,4 @@ urlpatterns = [
     path('produk/', include('produk.urls')),
     path('karyawan/', include('karyawan.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
