1e35c7ce05c59c139a5edac1c799e926bb203403
Muhammad Akhta <akhtacaesar@gmail.com>
2022-07-12 16:25:47 +0700

    Minor Static & Interface Rework

--- umkmbookeeping/static/js/stats.js
+++ umkmbookeeping/static/js/stats.js
@@ -25,16 +25,16 @@ const renderChart=(data,labels)=>{
                 label: "Pembukuan 6 Bulan Terakhir",
                 data: data,
                 backgroundColor: [
-                    'rgba(0,128,0,0.2)',
                     'rgba(255, 0, 0, 0.2)',
+                    'rgba(0,128,0,0.2)',
                     'rgba(54, 162, 235, 0.2)',
                     'rgba(75, 192, 192, 0.2)',
                     'rgba(153, 102, 255, 0.2)',
                     'rgba(255, 159, 64, 0.2)'
                 ],
                 borderColor: [
-                    'rgba(0,128,0,1)',
                     'rgba(255, 99, 132, 1)',
+                    'rgba(0,128,0,1)',
                     'rgba(54, 162, 235, 1)',
                     'rgba(75, 192, 192, 1)',
                     'rgba(153, 102, 255, 1)',
