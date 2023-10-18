8c279a197131afdc4676d451c2fe04ee934d4f5d
Muhammad Akhta <akhtacaesar@gmail.com>
2023-10-18 18:38:45 +0700

    Initial Redeployement Update

--- authentication/migrations/superuser.py
+++ authentication/migrations/superuser.py
@@ -2,16 +2,18 @@
 from __future__ import unicode_literals
 from django.db import migrations
 from django.contrib.auth.admin import User
+import os
 
 def create_superuser(apps, schema_editor):
     superuser = User()
     superuser.is_active = True
     superuser.is_superuser = True
     superuser.is_staff = True
-    superuser.username = 'admin'
-    superuser.email = 'admin@admin.com'
-    superuser.set_password('admin')
+    superuser.username = os.environ.get('SUPERUSER_NAME')
+    superuser.email = os.environ.get('SUPERUSER_EMAIL'),
+    superuser.set_password(os.environ.get('SUPERUSER_PASSWORD'),)
     superuser.save()
+
 
 class Migration(migrations.Migration):
 
